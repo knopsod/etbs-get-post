6d98545309839efa176efcf3e65664c68b2ad958
Noppadon Sodram <knopsod@gmail.com>
2018-09-03 19:02:20 +0700

    permissions CRUD ok and assign role to permission ok

--- routes/v1/etbsPermissions.js
+++ routes/v1/etbsPermissions.js
@@ -49,12 +49,27 @@ router.get('/edit/:permission/:profileid/:perm_type', function(req, res, next) {
   var profileid = req.params.profileid;
   var perm_type = req.params.perm_type;
 
-  res.render('v1/etbsPermissionsForm', {
-    action: '/etbs-permissions/update',
-    permission: permission,
-    profileid: profileid,
-    perm_type: perm_type
-  });
+  var conn = database.getConnection();
+
+  if (conn) {
+    var sql = "SELECT rolename, profileid FROM roles WHERE profileid = ? LIMIT 1 OFFSET 0";
+    var conditions = [profileid];
+
+    conn.query(sql, conditions, function (err, result) {
+      var rolename = result.length ? result[0].rolename : 'Not yet assign ROLE';
+
+      res.render('v1/etbsPermissionsForm', {
+        action: '/etbs-permissions/update',
+        permission: permission,
+        profileid: profileid,
+        perm_type: perm_type,
+        rolename: rolename
+      });
+
+      conn.end();
+    });
+  }
+
 });
 
 router.post('/update', function(req, res, next) {
@@ -95,12 +110,26 @@ router.get('/remove/:permission/:profileid/:perm_type', function(req, res, next)
   var profileid = req.params.profileid;
   var perm_type = req.params.perm_type;
 
-  res.render('v1/etbsPermissionsForm', {
-    action: '/etbs-permissions/delete',
-    permission: permission,
-    profileid: profileid,
-    perm_type: perm_type
-  });
+  var conn = database.getConnection();
+
+  if (conn) {
+    var sql = "SELECT rolename, profileid FROM roles WHERE profileid = ? LIMIT 1 OFFSET 0";
+    var conditions = [profileid];
+
+    conn.query(sql, conditions, function (err, result) {
+      var rolename = result.length ? result[0].rolename : 'Not yet assign ROLE';
+
+      res.render('v1/etbsPermissionsForm', {
+        action: '/etbs-permissions/delete',
+        permission: permission,
+        profileid: profileid,
+        perm_type: perm_type,
+        rolename: rolename
+      });
+
+      conn.end();
+    });
+  }
 });
 
 router.post('/delete', function(req, res, next) {
@@ -125,11 +154,66 @@ router.post('/delete', function(req, res, next) {
 });
 
 router.get('/roles/:permission/:profileid/:perm_type', function(req, res, next) {
-  res.send('respond etbs-permissions');
+  var permission = req.params.permission;
+  var profileid = req.params.profileid;
+  var perm_type = req.params.perm_type;
+  var rolename = '';
+
+  var conn = database.getConnection();
+
+  if (conn) {
+    var sql = 'SELECT rolename, profileid FROM roles WHERE profileid = ? LIMIT 1 OFFSET 0';
+    var conditions = [profileid];
+
+    conn.query(sql, conditions, function (err, result) {
+      
+      if (result) {
+        rolename = result.length ? result[0].rolename : 'Not yet assign ROLE';
+        
+        var sql = 'SELECT rolename, profileid FROM roles';
+        
+        conn.query(sql, function (err, roleResult) {
+          res.render('v1/etbsPermissionRoles', 
+            {
+              permission: permission,
+              profileid: profileid,
+              perm_type: perm_type,
+              rolename: rolename,
+              roles: roleResult
+            }
+          );
+          conn.end();
+        });
+      }
+    });
+  }
 });
 
 router.post('/roles/update', function(req, res, next) {
-  res.send('respond etbs-permissions');
+  var permission = req.body.permission;
+  var profileid = req.body.profileid;
+  var perm_type = req.body.perm_type;
+  var roleProfileid = req.body.roleProfileid;
+
+  var conn = database.getConnection();
+
+  if (conn) {
+
+    var sql = "UPDATE permissions SET ? "
+      + "WHERE permission = ? AND profileid = ? AND perm_type = ? ";
+    var setditions = [
+      { profileid: roleProfileid },
+      permission,
+      profileid,
+      perm_type
+    ];
+
+    conn.query(sql, setditions, function (err, result) {
+      res.redirect('/etbs-permissions/roles/' + permission + '/' + roleProfileid + '/' + perm_type);
+      conn.end();
+    });
+
+  }
 });
 
 module.exports = router;
